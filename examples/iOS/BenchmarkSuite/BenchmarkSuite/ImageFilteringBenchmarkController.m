37ab32b3012d7a3805863e055b1503e71e42fcef
zhangjiemin <yons@zhangjiemindeiMac.local>
2020-04-02 19:20:33 +0800

    更新注释

--- examples/iOS/BenchmarkSuite/BenchmarkSuite/ImageFilteringBenchmarkController.m
+++ examples/iOS/BenchmarkSuite/BenchmarkSuite/ImageFilteringBenchmarkController.m
@@ -170,10 +170,13 @@
 
     //GPUImagePicture 继承于GPUImageOutput，此实例维护了一系列的GPUImageinput协议，每一个GPUImageInput都维护了一个texture
     GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:imageToProcess];
+    //GPUImageSepiaFilter是一种滤镜
     GPUImageSepiaFilter *stillImageFilter = [[GPUImageSepiaFilter alloc] init];
-    
+    //将滤镜加入
     [stillImageSource addTarget:stillImageFilter];
+    //滤镜调用useNextFrameForImageCapture
     [stillImageFilter useNextFrameForImageCapture];
+    //执行滤镜
     [stillImageSource processImage];
     
     UIImage *currentFilteredVideoFrame = [stillImageFilter imageFromCurrentFramebuffer];
